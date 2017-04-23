13acbc57e73807e8f644ad44e8318c7b36a73f1c
Sidharth <bansal.sidharth2996@gmail.com>
2017-04-23 23:26:38 +0530

    Added node class

---
+++ LinkedList/Linkedlist.rb
@@ -0,0 +1,28 @@
+class LinkedList
+	def append
+	end
+	def prepend
+	end
+	def size
+	end
+	def head
+	end
+	def tails
+	end
+	def at(index)
+	end
+	def pop
+	end
+	def contains?
+	end
+	def find(data)
+	end
+	def to_s
+	end
+	def insert_at(index)
+	end
+	def remove_at(index)
+	end
+
+end
+
---
+++ LinkedList/Node.rb
@@ -0,0 +1,7 @@
+class Node
+	attr_accessor :next_node , :value
+	def initialize(value)
+		@value = value
+		@next_node = nil
+	end
+end
